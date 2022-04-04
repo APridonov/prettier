f120117e2170f47733a73f524c96a9b627c776ff
APridonov <102980865+APridonov@users.noreply.github.com>
2022-04-04 21:57:30 +0600

    Exploring Java. Hello World application.

---
+++ src/main/java/pro/almiron/PrettierApp.java
@@ -0,0 +1,10 @@
+package pro.almiron;
+
+public class PrettierApp {
+
+  public static void main(String[] args) {
+    TextPrint textPrint = new TextPrint();
+    textPrint.print("Hello World!");
+  }
+
+}
---
+++ src/main/java/pro/almiron/TextPrint.java
@@ -0,0 +1,9 @@
+package pro.almiron;
+
+public class TextPrint {
+
+  public void print(String text) {
+    System.out.println(text);
+  }
+
+}
